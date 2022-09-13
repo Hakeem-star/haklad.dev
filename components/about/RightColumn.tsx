ab20aa95288b5eee0ebc188d86cccf30793b76bc
Hakeem Ladejobi <haklad1@googlemail.com>
2022-09-13 18:49:11 +0100

    Layout and style info section

--- components/about/RightColumn.tsx
+++ components/about/RightColumn.tsx
@@ -4,12 +4,28 @@ import styled from "styled-components";
 import { ColumnHeader, FancyBorderWrapper } from "./shared/ui";
 import { colors } from "../../constants";
 
+const Intro = styled.p`
+  margin-bottom: 20px;
+`;
+
+const JobDetail = styled.p`
+  width: calc(100% - 5rem);
+  margin-left: 2rem;
+  margin-bottom: 30px;
+`;
+
+const Job = styled.p`
+  max-width: 80%;
+  color: ${colors.witcher_text_brighter_gold};
+  font-size: 15px;
+  margin-bottom: 0.5rem;
+`;
+
 const Info = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 20px;
   overflow: auto;
-  padding-right: 5px;
+  padding: 5px;
 
   color: ${colors.bestiary_details_border_active};
 
@@ -38,36 +54,49 @@ export const RightColumn = (props: Props) => {
       <FancyBorderWrapper>
         <ColumnHeader style={{ marginBottom: 10 }}>Experience</ColumnHeader>
         <Info>
-          <p>
+          <Intro>
             Experienced in many aspects of the web and always trying to learn
             and improve. Worked several technical roles in the digital
             advertising sector over a period of 5 years. Self-taught JavaScript
             developer with a combination of skills that allow the development of
             creative solutions for clients and internal teams.
-          </p>
-          <p>
-            NeverBland / FullStack JavaScript Developer London – January 2021 –
-            Now Building data-rich web apps using React, Typescript, Prisma
-            &amp; GraphQL. Collaborating with the Client, Designers, Product
-            Managers, and other Engineers to deliver compelling user-facing
-            products.
-          </p>
-          <p>
-            MediaCom / Senior Technology Engineer London – March 2018 – Dec 2020
-            Troubleshooting and improving processes where possible. Using
-            JavaScript, I created a chrome extension we used to add
-            functionalities to DoubleClick Campaign Manager as well as using the
-            Google API to create other tools and automate tasks
-          </p>
-          <p>
-            Viant / Tech Producer November 2015 - February 2018 Gained a
-            promotion from a Junior ad trafficker to becoming an important part
-            of the company as a Tech producer in just over the space of a year
-            through hard work and a willingness to learn. Assist internal teams
-            with Ad ops, programmatic and business intel related issues. Provide
-            QA support for operations Using Google Big Query (SQL) and Tableau
-            to mine data as well as bridge gaps between data systems
-          </p>
+          </Intro>
+          <div>
+            <Job>
+              NeverBland / FullStack JavaScript Developer London – January 2021
+              – Now
+            </Job>
+            <JobDetail>
+              Building data-rich web apps using React, Typescript, Prisma &amp;
+              GraphQL. Collaborating with the Client, Designers, Product
+              Managers, and other Engineers to deliver compelling user-facing
+              products.
+            </JobDetail>
+          </div>
+          <div>
+            <Job>
+              MediaCom / Senior Technology Engineer London – March 2018 – Dec
+              2020
+            </Job>
+            <JobDetail>
+              Troubleshooting and improving processes where possible. Using
+              JavaScript, I created a chrome extension we used to add
+              functionalities to DoubleClick Campaign Manager as well as using
+              the Google API to create other tools and automate tasks
+            </JobDetail>
+          </div>
+          <div>
+            <Job>Viant / Tech Producer November 2015 - February 2018</Job>
+            <JobDetail>
+              Gained a promotion from a Junior ad trafficker to becoming an
+              important part of the company as a Tech producer in just over the
+              space of a year through hard work and a willingness to learn.
+              Assist internal teams with Ad ops, programmatic and business intel
+              related issues. Provide QA support for operations Using Google Big
+              Query (SQL) and Tableau to mine data as well as bridge gaps
+              between data systems
+            </JobDetail>
+          </div>
         </Info>
       </FancyBorderWrapper>
     </ColumnWrapper>
