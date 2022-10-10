bc51ff1035a11b733850ce3d5720881e5ee20840
Hakeem Ladejobi <haklad1@googlemail.com>
2022-10-10 14:22:34 +0100

    Add easing on camera movement

--- components/experiments/Canvas.tsx
+++ components/experiments/Canvas.tsx
@@ -6,7 +6,7 @@ import {
   MapControls,
 } from "@react-three/drei";
 import { MapControls as MapControlsImpl } from "three-stdlib";
-import { Canvas as ThreeCanvas, useThree } from "@react-three/fiber";
+import { Canvas as ThreeCanvas, useFrame, useThree } from "@react-three/fiber";
 import {
   DepthOfField,
   EffectComposer,
@@ -14,10 +14,11 @@ import {
   Vignette,
 } from "@react-three/postprocessing";
 import { useControls } from "leva";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 import { DirectionalLight, Vector3, WebGLCubeRenderTarget } from "three";
 import { Bridge } from "./Bridge";
+import { lerp, mapLinear } from "three/src/math/MathUtils";
 
 const cameraDefaultPosition = [0, 0, 5];
 
@@ -54,128 +55,14 @@ function drawBridge(ctx: CanvasRenderingContext2D, img: HTMLImageElement) {
 type Props = {};
 
 const Canvas = (props: Props) => {
-  // const canvasRef = useRef<HTMLCanvasElement>(null);
-  // const bridgeRef = useRef<HTMLImageElement | null>(null);
-  // const skylinesRef = useRef<HTMLImageElement | null>(null);
-
-  // const getContext = () => {
-  //   const canvas = canvasRef.current;
-
-  //   const ctx = canvas?.getContext("2d");
-  //   return ctx;
-  // };
-
-  // // setup
-  // useEffect(() => {
-  //   const ctx = getContext();
-  //   if (!ctx) return;
-
-  //   ctx.canvas.width = window.innerWidth;
-  //   ctx.canvas.height = window.innerHeight;
-  //   let x = window.innerWidth / 2;
-  //   let y = ctx.canvas.height / 2;
-  //   ctx.fillStyle = "black";
-  //   ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-  // }, []);
-
-  // // clouds
-  // useEffect(() => {
-  //   const canvas = canvasRef.current;
-  //   if (!canvas) return;
-  //   perlinNoise(
-  //     canvas,
-  //     { r: 0, g: 0, b: 0 },
-  //     { r: 119, g: 200, b: 255 },
-  //     1,
-  //     1.4
-  //   );
-  // }, []);
-
-  // // skyline
-  // useEffect(() => {
-  //   const ctx = getContext();
-  //   if (!ctx) return;
-
-  //   const img = new Image();
-  //   img.onload = () => {
-  //     skylinesRef.current = img;
-
-  //     drawSkyline(ctx, img);
-  //   };
-  //   img.src = "./images/new-york-skyline.png";
-
-  //   return () => {};
-  // }, []);
-
-  // // bridge
-  // useEffect(() => {
-  //   const ctx = getContext();
-  //   if (!ctx) return;
-
-  //   const img = new Image();
-
-  //   img.onload = () => {
-  //     bridgeRef.current = img;
-
-  //     drawBridge(ctx, img);
-  //   };
-  //   img.src = "./images/pngfind.com-bridge-png-517681.png";
-
-  //   return () => {};
-  // }, []);
-
-  //   // rain
-  //   useEffect(() => {
-  //     const ctx = getContext();
-  //     if (!ctx) return;
-
-  //     const rain = generateRain(ctx);
-
-  //     const run = () => {
-  //       // clear canvas
-  //       //   ctx.fillStyle = "white";
-  //       ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-
-  //       if (skylinesRef.current) {
-  //         drawSkyline(ctx, skylinesRef.current);
-  //       }
-  //       if (bridgeRef.current) {
-  //         drawBridge(ctx, bridgeRef.current);
-  //       }
-  //       rain();
-  //       requestAnimationFrame(run);
-  //     };
-
-  //     run();
-  //   }, []);
-
-  // var geometry = new THREE.SphereGeometry(0.5, 32, 32);
-  // var texture = new THREE.TextureLoader().load( "data:image/jpeg;base64,/9j/4AAQSkZJRgA--(truncated-for-example)--BAgEASABIAAD" );
-  // var material = new THREE.MeshBasicMaterial( { map: texture } );
-  // var mesh = new THREE.Mesh(geometry, material);
-  // scene.add(mesh);
-
-  // const { focusDistance, focalLength, bokehScale } = useControls({
-  //   focusDistance: {
-  //     min: 0,
-  //     max: 4,
-  //     value: 2,
-  //   },
-  //   focalLength: {
-  //     min: 0,
-  //     max: 1,
-  //     value: 0.1,
-  //   },
-  //   bokehScale: {
-  //     min: 0,
-  //     max: 10,
-  //     value: 2,
-  //   },
-  // });
-
   return (
     <Wrapper>
-      <ThreeCanvas camera={{ position: cameraDefaultPosition }}>
+      <ThreeCanvas
+        camera={{
+          // @ts-ignore
+          position: cameraDefaultPosition,
+        }}
+      >
         <Scene />
         <EffectComposer>
           <DepthOfField
@@ -183,8 +70,8 @@ const Canvas = (props: Props) => {
             focalLength={0.6}
             bokehScale={1.3}
           />
-          {/* <Noise opacity={0.02} /> */}
-          {/* <Vignette eskil={false} offset={0.1} darkness={1.1} /> */}
+          <Noise opacity={0.02} />
+          <Vignette eskil={false} offset={0.1} darkness={0.3} />
         </EffectComposer>
       </ThreeCanvas>
     </Wrapper>
@@ -206,73 +93,54 @@ function Scene() {
     },
   });
 
-  // useEffect(() => {
-  //   camera.position.setX(panX);
-  //   camera.position.setZ(panZ);
-  // }, [camera, panX, panZ]);
+  useTexture("./images/rural_winter_roadside.jpg", (textures) => {
+    const skyBoxTexture = Array.isArray(textures) ? textures[0] : textures;
 
-  const skyBox = useTexture(
-    "./images/rural_winter_roadside.jpg",
-    (textures) => {
-      const skyBoxTexture = Array.isArray(textures) ? textures[0] : textures;
+    const rt = new WebGLCubeRenderTarget(skyBoxTexture.image.height);
+    rt.fromEquirectangularTexture(gl, skyBoxTexture);
+    // scene.background = rt.texture;
+  });
 
-      const rt = new WebGLCubeRenderTarget(skyBoxTexture.image.height);
-      rt.fromEquirectangularTexture(gl, skyBoxTexture);
-      scene.background = rt.texture;
-    }
-  );
-
-  const [skyline, bridge] = useTexture(
-    [
-      "./images/new-york-skyline-edited.png",
-      "./images/pngfind.com-bridge-png-517681.png",
-      "./images/rural_winter_roadside.jpg",
-    ],
-    (textures) => {
-      if (Array.isArray(textures)) {
-        const skyBoxTexture = textures[2];
-
-        const rt = new WebGLCubeRenderTarget(skyBoxTexture.image.height);
-        rt?.fromEquirectangularTexture(gl, skyBoxTexture);
-        scene.background = rt.texture;
-      }
-    }
-  );
+  const [skyline, bridge] = useTexture([
+    "./images/new-york-skyline-edited.png",
+    "./images/pngfind.com-bridge-png-517681.png",
+  ]);
 
   const skylineRatio = skyline.image.width / skyline.image.height;
   const bridgeRatio = bridge.image.width / bridge.image.height;
   const light = useRef<DirectionalLight>(null);
   const controlsRef = useRef<MapControlsImpl>(null);
 
-  useEffect(() => {
+  useFrame((state) => {
+    // mouse values are 0 - 1
+    const xPos = state.mouse.x;
+    const yPos = state.mouse.y;
+
+    const camX = mapLinear(xPos, 0, 1, -0.1, 0.02);
+    const camZ = mapLinear(yPos, 0, 1, -0.04, 0.02);
+
     const controls = controlsRef.current;
     if (!controls) return;
 
-    controls?.addEventListener("change", function () {
-      if (controls.target.x < -0.15) {
-        controls.target.x = -0.15;
-        camera.position.x = -0.15;
-      } else if (controls.target.x > 0.15) {
-        controls.target.x = 0.15;
-        camera.position.x = 0.15;
-      }
+    // ease the movement of the camera
+    controls.target.x = lerp(controls.target.x, camX, 0.1);
+    camera.position.x = lerp(camera.position.x, camX, 0.1);
 
-      if (controls.target.z < -0.02) {
-        controls.target.z = -0.02;
-        camera.position.z = cameraDefaultPosition[2] + -0.02;
-      } else if (controls.target.z > 0.02) {
-        controls.target.z = 0.02;
-        camera.position.z = cameraDefaultPosition[2] + 0.02;
-      }
-    });
-  }, []);
+    controls.target.z = lerp(controls.target.z, camZ, 0.1);
+    camera.position.z = lerp(
+      camera.position.z,
+      cameraDefaultPosition[2] + camZ,
+      0.1
+    );
+  });
+
   return (
     <>
       <MapControls
         enableDamping
         dampingFactor={0.05}
         ref={controlsRef}
-        enableRotate={false}
+        enabled={false}
       />
       <fog attach="fog" args={["#b9b9b9", 1, 20]} />
       <ambientLight intensity={1} />
